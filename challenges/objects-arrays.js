ea767cc347e7e847c0d84628b7aad500d1c769de
Joel Bartlett <joel@murbar.com>
2019-01-25 11:38:27 -0500

    Add dino objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,6 +12,30 @@
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
 
+const ty = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceious'
+}
+
+const st = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
+
+const ve = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceious'
+}
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
